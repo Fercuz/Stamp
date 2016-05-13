59de1f013ed15d76e1a278e89b6069def4cea5c8
Fercuz <fercuz@gmail.com>
2016-05-13 15:41:18 -0500

    Actualización.

--- Stamping/admin.py
+++ Stamping/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import *
+from django.db import models
 
 class ColorAdmin(admin.ModelAdmin):
 	list_display = ('nombre','referencia')
@@ -39,7 +39,6 @@ class PerfilAdmin(admin.ModelAdmin):
 class PersonaAdmin(admin.ModelAdmin):
 	list_display = ('cedula','nombre','apellido','fecha_nacimiento','telefono','movil','email')
 	search_fields = ('cedula','nombre','apellido','fecha_nacimiento','telefono','movil','email')
-	actions = [export_as_csv]
 
 class ProductoAdmin(admin.ModelAdmin):
 	list_display = ('nombre','valor_venta','valor_produccion','cantidad_existente','descripcion')
@@ -81,35 +80,6 @@ admin.site.register(Tipotela,TipotelaAdmin)
 #     class Meta:
 #         model = Color
 
-# from django.contrib import admin
-# from models import Enlace, Categoria, Agregador
-
-# from actions import export_as_csv
-
-# class EnlaceAdmin(admin.ModelAdmin):
-#     list_display = ('enlace', 'titulo', 'categoria', 'imagen_voto', 'es_popular')
-#     list_filter = ('categoria', 'usuario',)
-#     search_fields = ('categoria__titulo', 'usuario__email')
-#     list_editable = ('titulo', 'enlace', 'categoria')
-#     list_display_links = ('es_popular',)
-#     actions = [export_as_csv]
-#     raw_id_fields = ('categoria', 'usuario')
-
-#     def imagen_voto(self, obj):
-#         url = obj.mis_votos_en_imagen_rosada()
-#         tag = '<img src="%s">' % url
-#         return tag
-#     imagen_voto.allow_tags = True
-#     imagen_voto.admin_order_field = 'votos'
-
-# class EnlaceInline(admin.StackedInline):
-#     model = Enlace
-#     extra = 3
-#     raw_id_fields = ('usuario',)
-
-# class CategoriaAdmin(admin.ModelAdmin):
-#     actions = [export_as_csv]
-#     inlines = [EnlaceInline]
 
 
 
