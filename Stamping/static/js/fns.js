ec8e2129e5a60e2f1ca9a4381d2287a906a785e7
Fercuz <fercuz@gmail.com>
2016-05-18 16:11:35 -0500

    Last update!

---
+++ Stamping/static/js/fns.js
@@ -0,0 +1,11 @@
+
+// function dis_per(){
+//   $('#source').attr({'disabled':true,'class':'inp_ctd inp_ctrt'});
+// }
+// function ena_per(){
+//   $('#source').removeAttr('disabled');
+// }
+
+function rdr(loc) {
+  location.href = loc;
+}
