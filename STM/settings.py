689351746c281ac4acb35bd8e81be25e9533311e
Fercuz <fercuz@gmail.com>
2016-05-19 10:33:43 -0500

    Actualización.

--- STM/settings.py
+++ STM/settings.py
@@ -94,6 +94,6 @@ STATIC_URL = "/static/"
 
 EMAIL_USE_TLS = True
 EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'fercuz@gmail.com'
-EMAIL_HOST_PASSWORD = 'anamari'
+EMAIL_HOST_USER = ''
+EMAIL_HOST_PASSWORD = ''
 EMAIL_PORT = 587
