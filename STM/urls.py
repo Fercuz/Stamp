a671ad2618e868c829014b13517536eac05e2869
Fercuz <fercuz@gmail.com>
2016-05-19 08:36:17 -0500

    Actualización.

--- STM/urls.py
+++ STM/urls.py
@@ -7,6 +7,7 @@ urlpatterns = (
     url(r'^admin/', admin.site.urls),
     url(r'^$', Stamping.views.index, name='index'),
     url(r'^local/', Stamping.views.local, name='local'),
-    #url(r'^contact/$', Stamping.views.contact, name='contact'),
+    url(r'^contacto/$', Stamping.views.contacto, name='contacto'),
+    url(r'^gracias/$', Stamping.views.gracias, name='gracias'),
 
 )
--- Stamping/views.py
+++ Stamping/views.py
@@ -5,9 +5,10 @@ from django.template import Context
 from datetime import datetime
 from django.shortcuts import render
 from django.shortcuts import render_to_response
-from Stamping.models import *
 from django.shortcuts import get_object_or_404
 from django.template.context import RequestContext
+#from django.shortcuts import HttpResponseRedirect
+from Stamping.forms import Formulario
 
 def handler404(request):
     response = render_to_response('404.html', {},context_instance=RequestContext(request))
@@ -21,6 +22,22 @@ def handler500(request):
 
 def index(request):
     return render_to_response('index.html', context_instance = RequestContext(request))
+
+def contacto(request):
+    if request.method == 'POST':
+        form = Formulario(request.POST)
+        if form.is_valid():
+            return render_to_response('gracias.html', context_instance=RequestContext(request))
+    else:
+        form = Formulario()
+
+    return render (request,'contacto.html', {
+        'form': form,
+    })
+
+def gracias(request):
+    html='<html><body><h1>Gracias por enviarnos su comentario!.</h1></body></html>'
+    return HttpResponse(html)
 
 def local(request):
     return render_to_response('home.html', context_instance = RequestContext(request))
