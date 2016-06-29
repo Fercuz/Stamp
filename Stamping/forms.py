50299579c5df59a7138233b2142c03abe500d3fd
Fercuz <fercuz@gmail.com>
2016-06-29 16:03:05 -0500

    Actualización.

--- Stamping/forms.py
+++ Stamping/forms.py
@@ -2,5 +2,11 @@ from django import forms
 
 class Formulario (forms.Form):
     email = forms.EmailField(label='',widget=forms.Textarea(attrs={'placeholder': 'Email'}))
-    asunto = forms.CharField(label='',widget=forms.Textarea(attrs={'placeholder': 'Asunto'}),max_length=100)
-    mensaje =  forms.CharField(label='',widget=forms.Textarea(attrs={'placeholder': 'Mensaje'}))
+    titulo = forms.CharField(label='',widget=forms.Textarea(attrs={'placeholder': 'Asunto'}),max_length=100)
+    texto =  forms.CharField(label='',widget=forms.Textarea(attrs={'placeholder': 'Mensaje'}))
+
+
+# class Formulario (forms.Form):
+#     email = forms.EmailField(label='',widget=forms.Textarea(attrs={'placeholder': 'Email'}))
+#     asunto = forms.CharField(label='',widget=forms.Textarea(attrs={'placeholder': 'Asunto'}),max_length=100)
+#     mensaje =  forms.CharField(label='',widget=forms.Textarea(attrs={'placeholder': 'Mensaje'}))
--- Stamping/views.py
+++ Stamping/views.py
@@ -9,7 +9,7 @@ from django.shortcuts import get_object_or_404
 from django.template.context import RequestContext
 # from django.shortcuts import HttpResponseRedirect
 from Stamping.forms import Formulario
-from django.core.mail import EmailMessage
+from django.core.mail import EmailMultiAlternatives,EmailMessage
 
 
 def handler404(request):
@@ -28,23 +28,28 @@ def index(request):
     return render_to_response('index.html', context_instance=RequestContext(request))
 
 def contacto(request):
+    info_send = False
+    email = ""
+    titulo = ""
+    texto = ""
+
     if request.method == 'POST':
         form = Formulario(request.POST)
         if form.is_valid():
-            # asunto = form.cleaned_data['asunto']
-            # mensaje = form.cleaned_data['mensaje']
-            # email = form.cleaned_data['email']
-            asunto = 'asunto'
-            mensaje = 'mensaje'
-            email = 'email'
-            mail = EmailMessage(asunto,mensaje,email)
-            mail.send()
+            info_send = True
+            email = form.cleaned_data['email']
+            titulo = form.cleaned_data['titulo']
+            texto = form.cleaned_data['texto']
 
-            return render_to_response('gracias.html', context_instance=RequestContext(request))
+            to_admin = 'soportestamping@gmail.com'
+            html_content = "Información recibida de [%s]<br><br>Mensaje:<br><br>%s"%(email,texto)
+            msg = EmailMultiAlternatives('Correo de Contacto', html_content,'from@server.com',[to_admin])
+            msg.attach_alternative(html_content,'text/html')
+            msg.send()
     else:
         form = Formulario()
-    return render_to_response('contacto.html', {'form': form}, context_instance=RequestContext(request))
-
+    ctx = {'form':form, 'email':email,'titulo':titulo, 'texto':texto,'info_send':info_send}
+    return render_to_response('contacto.html',ctx, context_instance=RequestContext(request))
 
 def gracias(request):
     html = '<html><body><h1>Gracias por enviarnos su comentario!.</h1></body></html>'
